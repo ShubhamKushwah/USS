2fbcedd3c1ee5d5faa4d33273025a7b68de907c3
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-12-24 10:29:30 +0530

    Created URL shortcode, and utility file

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-
+from .utils import code_generator, create_shortcode
 
 class ShortUrl(models.Model):
 	url = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=16, unique=True)
+	shortcode = models.CharField(max_length=16, unique=True, blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return str(self.url)
 
 	def __unicode__(self):
 		return str(self.url)
+
+	def save(self, *args, **kwargs):
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode = create_shortcode(self)
+		super(ShortUrl, self).save(*args, **kwargs)
