e6081c968aef6f086514d1f1e86f2dcb6349b084
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-12-24 10:52:42 +0530

    Added management and commands

---
+++ src/shortener/management/commands/refreshcodes.py
@@ -0,0 +1,14 @@
+# refreshcodes is the command name & this file's name and is used as:
+# python manage.py refreshcodes
+
+from django.core.management.base import BaseCommand, CommandError
+from shortener.models import ShortUrl
+
+class Command(BaseCommand):
+    help = 'Refreshes all the shortcodes, in the database'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('poll_id', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+    	return ShortUrl.objects.refresh_shortcodes()
--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 from .utils import code_generator, create_shortcode
 
+class ShortURLManager(models.Manager):
+	def all(self, *args, **kwargs):
+		qs = super(ShortURLManager, self).all(*args, **kwargs)
+		qs = qs.filter(active=True)
+		return qs
+
+	def refresh_shortcodes(self):
+		qs = ShortUrl.objects.filter(id_gte=1)	# gte=greater than or equal to
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			q.save()
+			new_codes += 1
+		return "New Codes made {i}".format(i=new_codes)
+
 class ShortUrl(models.Model):
 	url = models.CharField(max_length=220)
 	shortcode = models.CharField(max_length=16, unique=True, blank=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+
+	objects = ShortURLManager()		# this is ShortUrl. -> objects <- .all()
 
 	def __str__(self):
 		return str(self.url)
