a0e41e8cacb21b4d5bc5d8769cfc94fd4f716347
Shubham Kushwah <shubhamkushwahsk@gmail.com>
2017-12-26 01:36:04 +0530

    changed the anchor tag styling to transparent border

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -40,6 +40,10 @@ class ShortUrl(models.Model):
 	def save(self, *args, **kwargs):
 		if self.shortcode is None or self.shortcode == "":
 			self.shortcode = create_shortcode(self)
+
+		if not "http" in self.url:
+			self.url = "http://" + self.url
+
 		super(ShortUrl, self).save(*args, **kwargs)
 
 	def get_short_url(self):
